f401f9f245693d7711eaba52881d1dda1f38e323
Robert Nowotniak <robert@nowotniak.com>
2008-02-27 13:37:58 +0000

    git-svn-id: file:///home/rob/www/git/svn-old/qclib@23 98c7cd80-c913-11dc-80dc-85fb3d3e7d31

---
+++ operators.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+
+from numpy import *
+
+
+S = (
+        (1.0/sqrt(5) * transpose(matrix([2, 1])), 1.0/sqrt(10) * transpose(matrix([3, 1]))),
+        (1.0/sqrt(20) * transpose(matrix([-2, 4])), 1.0/sqrt(40) * transpose(matrix([2, -6]))),
+        )
+
+print S[0][0]
+print S[1][0]
+
+print dot(transpose(S[0][0]), S[1][0])
+
+def random_unitary_matrix(n):
+    z = (random.randn(n,n) + 1j * random.randn(n,n)) / sqrt(2.0)
+    q,r = linalg.qr(z)
+    d = diagonal(r)
+    ph = d / absolute(d)
+    q = multiply(q, ph, q)
+    return q
+
+print random_unitary_matrix(3)
+
