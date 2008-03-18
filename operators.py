692c01f9adcb7ea12b926fc718449e6be36c4299
Robert Nowotniak <robert@nowotniak.com>
2008-03-18 03:45:24 +0000

    git-svn-id: file:///home/rob/www/git/svn-old/qclib@24 98c7cd80-c913-11dc-80dc-85fb3d3e7d31

--- operators.py
+++ operators.py
@@ -15,11 +15,15 @@ print dot(transpose(S[0][0]), S[1][0])
 
 def random_unitary_matrix(n):
     z = (random.randn(n,n) + 1j * random.randn(n,n)) / sqrt(2.0)
+    # for real matrix
+    # z = (random.randn(n,n) + 1j * random.randn(n,n)) / sqrt(2.0)
     q,r = linalg.qr(z)
     d = diagonal(r)
     ph = d / absolute(d)
     q = multiply(q, ph, q)
-    return q
+    return matrix(q)
 
-print random_unitary_matrix(3)
+a = random_unitary_matrix(3)
+print a
+print (abs(a*a.H - eye(3)) > 10e-6).any()
 
