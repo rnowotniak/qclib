ee2b9a46b3083f33b4a26b20de26d126eebb7510
Robert Nowotniak <robert@nowotniak.com>
2017-10-10 01:08:46 +0200

    GNU GPL license header added

--- gpent3.py
+++ gpent3.py
@@ -3,7 +3,23 @@
 # Genetic Programming algorithm for for evolving
 # 3-qubit entanglement production quantum circuit
 #
-# Copyright (C) 2008   Robert Nowotniak <robert@nowotniak.com>
+# Copyright (C) 2006   Robert Nowotniak <rnowotniak@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+
 #
 # based on:
 # [Rub00] Ben I. P. Rubinstein. Evolving quantum circuits using genetic programming
